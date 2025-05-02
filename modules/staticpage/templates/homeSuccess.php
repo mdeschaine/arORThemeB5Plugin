267bed72d1c55182e41a5297d621032f769e4d86
Mathieu <mathieu.deschaine@gmail.com>
2025-05-02 16:09:12 -0700

    fix col widths

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -55,7 +55,7 @@
         <div class="col-md-4">
         <img src="/plugins/arORB5Plugin/images/OrgLot146_b06_SalemCapital_4.jpg" class="img-fluid rounded-start" alt="...">
         </div>
-        <div class="col-md-8">
+        <div class="col-md-4">
         <div class="card-body text-wrap">
             <h4 class="card-title">Digitizing the Oregon Story: Creating Access to Significant Legal and Political Oral Histories</h4>
             <p class="card-text">Listen to 212 Oral History interviews of significant legal and political figures, originally created on analog audio and video tapes and previously available only onsite in the OHS Research Library.</p>
